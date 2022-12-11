96a581f4a050e30f4241eb8f05841c17f18a3566
Vassiliy <I-IvanZakharov@yandex.ru>
2022-12-11 06:00:33 +0300

    Adding favicon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DnD Elegy',
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
